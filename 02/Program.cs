6ba859b2cea353105e0356240b3f6a71ab9107a1
Danya404 <voloshinden98@gmail.com>
2023-03-20 01:42:21 +0300

    2

---
+++ 02/Program.cs
@@ -0,0 +1,7 @@
+﻿int num = new Random().Next(10, 100);
+Console.WriteLine($"Случайное число из отрезка от 10 до 99 -> {num}");
+int firstnum = num / 10;
+int secondnum = num % 10;
+if (firstnum > secondnum) Console.WriteLine($"Наибольшая цифра числа -> {firstnum}");
+else Console.WriteLine($"Наибольшая цифра числа -> {secondnum}");
+ 
