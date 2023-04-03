af1530a1011699245df90b5ce7c217d4ae08cfa6
Danya404 <voloshinden98@gmail.com>
2023-04-03 16:57:12 +0300

    седьмой семинар

---
+++ 07/Program.cs
@@ -0,0 +1,160 @@
+﻿// int[,] CreateMatrix(int rows, int columns, int min, int max)
+// {
+//     int[,] matrix = new int[rows, columns];
+//     Random rnd = new Random();
+
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i, j] = rnd.Next(min, max + 1);
+//         }
+        
+//     }
+//     return matrix;
+// }
+
+// void PrintMatrix(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             Console.Write($"{matrix[i, j], 3} ");
+//         }
+//         Console.WriteLine();
+//     } 
+// }
+
+// int[,] array2d = CreateMatrix(3, 4, 1, 100);
+// PrintMatrix(array2d);
+
+// int[,] CreateMatrix(int rows, int columns)
+// {
+//     int[,] matrix = new int[rows, columns];
+
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i, j] = i + j;
+//         }
+        
+//     }
+//     return matrix;
+// }
+
+// void PrintMatrix(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             Console.Write($"{matrix[i, j], 3} ");
+//         }
+//         Console.WriteLine();
+//     } 
+// }
+// int[,] array2d = CreateMatrix(3, 4);
+// PrintMatrix(array2d);
+
+// Задайте двумерный массив. Найдите элементы, у
+// которых оба индекса чётные, и замените эти элементы на их
+// квадраты.
+
+// int[,] CreateMatrix(int rows, int columns, int min, int max)
+// {
+//     int[,] matrix = new int[rows, columns];
+//     Random rnd = new Random();
+
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i, j] = rnd.Next(min, max + 1);
+//         }
+        
+//     }
+//     return matrix;
+// }
+
+// void PrintMatrix(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             Console.Write($"{matrix[i, j], 3} ");
+//         }
+//         Console.WriteLine();
+//     } 
+// }
+
+// void ReplaceEven(int[,] matrix)
+// {
+// for (int i = 0; i < matrix.GetLength(0); i+= 2)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j+= 2)
+//         {
+//             matrix[i, j] *= matrix[i, j];
+//         }
+//     } 
+// }
+
+// int[,] array2d = CreateMatrix(3, 4, 1, 10);
+// PrintMatrix(array2d);
+// ReplaceEven(array2d);
+// PrintMatrix(array2d);
+
+
+// Задайте двумерный массив. Найдите сумму
+// элементов, находящихся на главной диагонали (с индексами
+// (0,0); (1;1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+int[,] CreateMatrix(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+        
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j], 3} ");
+        }
+        Console.WriteLine();
+    } 
+}
+
+int DiagonalSum(int[,] matrix)
+{
+    int result = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        result += matrix[i, i];  
+    } 
+    return (result);
+}
+
+int[,] array2d = CreateMatrix(3, 3, 1, 10);
+PrintMatrix(array2d);
+int result = DiagonalSum(array2d);
+Console.WriteLine($"Сумма чисел главной диагонали равна {result}");
